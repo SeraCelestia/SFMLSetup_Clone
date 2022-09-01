79052f74d76b68d4f73aef88f812c3d5c854dc0f
SeraCelestia <seraphir@outlook.com>
2022-09-01 15:54:38 +1200

    Add project files.

---
+++ SFMLSetup/main.cpp
@@ -0,0 +1,37 @@
+// Library includes
+#include <iostream>
+#include <vector>
+#include "SFML/Graphics.hpp"
+
+// Local includes
+
+int main()
+{
+    // Window init1
+	sf::RenderWindow window(sf::VideoMode(1280, 720), "Default Name");  // Set window size
+    window.setPosition(sf::Vector2i(600, 40));                          // Set window position
+    window.requestFocus();                                              // Focus on window
+
+    // Main window loop
+    while (window.isOpen())
+    {
+
+    }
+    
+    // Main even poll loop
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+
+        if (event.type == sf::Event::Closed)
+        {
+            window.close();
+            // delete stuff
+        }
+
+    }
+
+	window.clear();
+	window.display();
+	return 0;
+}
